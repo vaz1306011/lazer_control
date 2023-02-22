b1947b3d5bb573fa8230fe6983a563e0bcebea0a
小飛 <vaz1306011@gmail.com>
2023-02-22 17:34:42 +0800

    新增setting_window

--- find_point.py
+++ find_point.py
@@ -198,6 +198,50 @@ def main():
     cap.release()
 
 
+def setting_window():
+    cv2.namedWindow("res")
+
+    nothing = lambda _: _
+    cv2.createTrackbar("r", "res", 0, 255, nothing)
+    cv2.createTrackbar("g", "res", 0, 255, nothing)
+    cv2.createTrackbar("b", "res", 0, 255, nothing)
+
+    r = 0
+    g = 0
+    b = 0
+
+    cap = cv2.VideoCapture("./video/pos2.MOV")
+    # cap = cv2.VideoCapture(0)
+    is_pause = False
+
+    while True:
+        if not is_pause:
+            rval, img = cap.read()
+            if not rval:
+                break
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        key = cv2.waitKey(10)
+        if key == 27:
+            break
+        elif key == ord("p"):
+            is_pause = not is_pause
+
+        maxVal = cv2.getTrackbarPos("min", "res")
+        minVal = cv2.getTrackbarPos("max", "res")
+        if minVal < maxVal:
+            edge = cv2.Canny(blur, 100, 200)
+            cv2.imshow("res", edge)
+        else:
+            edge = cv2.Canny(blur, minVal, maxVal)
+            cv2.imshow("res", edge)
+
+    print(minVal, maxVal)
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
     main()
     cv2.destroyAllWindows()
